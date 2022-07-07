d14115c2264b2c9ed054794be790680e23dfb9b5
Bojan Mićić <bojan5683@gmail.com>
2022-07-07 15:59:49 +0200

    Second Commit

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -5,27 +5,37 @@ import './App.css';
 function App() {
 
   const [todos, setTodos] = useState([]); 
-  const [number, setNumber] = useState(1);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-  fetch(`https://jsonplaceholder.typicode.com/todos/?_limit=${number}`)
+  fetch(`https://jsonplaceholder.typicode.com/todos`)
   .then(response => response.json())
   .then(json => setTodos(JSON.parse(JSON.stringify(json))))
-  }, [number]);
+  }, []);
 
 
   return (
       <>
       <div className='input'>
-      <input type="number" onChange={e => (setNumber(e.target.value))} />
+      <input type="text" placeholder='Search...' onChange={e => (setSearch(e.target.value))} />
       </div>
       <div className='container'>
-      <ul className='id'>{todos.map((todo) => {
+      <ul className='id'>{todos.filter((todo) => {
+        if(search === "") {
+          return todo
+        } else if (todo.title.toLowerCase().includes(search.toLowerCase()) || todo.id === parseInt(search)){
+          return todo
+        }
+      })
+      .map((todo) => {
         return(
         <li>{todo.userId}
         <li className='title'>
-          {todo.title}
+            {todo.title}
         </li>
+        <li className='sort'>
+            {todo.id}
+          </li>
         </li>
         )
       })}
